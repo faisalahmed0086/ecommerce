45b15a83663e8b5567ee93de6b8dda7db053282c
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-02-18 00:46:37 +0600

    template context

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-	return render(request,"home_page.html",{})
+	context = {
+	"title":"Hello World!",
+	"content":"Welcome to the homepage.",
+	}
+	return render(request,"home_page.html", context)
+
+
+def about_page(request):
+	context = {
+		"title":"about page",
+		"content":" Welcome to the about page.",
+	}
+	return render(request,"home_page.html", context)
+
+def contact_page(request):
+	context = {
+		"title":"contact",
+		"content":" Welcome to the contact page.",
+	}
+	return render(request,"home_page.html", context)
