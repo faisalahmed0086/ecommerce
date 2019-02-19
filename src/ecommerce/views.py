a4c779be9f4d13caac5b6696c8ad1aee504b3ff0
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-02-20 00:11:25 +0600

    User login

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
 	context = {
@@ -33,3 +34,36 @@ def contact_page(request):
 	# 	print(request.POST.get('email'))
 	# 	print(request.POST.get('content'))
 	return render(request,"contact/view.html", context)
+
+def login_page(request):
+	form = LoginForm(request.POST or None)
+	context = {
+		"form": form
+	}
+	print("User logged in")
+	#print(request.user.is_authenticated())
+	if form.is_valid():
+		print(form.cleaned_data)
+		username = form.cleaned_data.get("username")
+		password = form.cleaned_data.get("password")
+		user = authenticate(request, username=username, password=password)
+		print(user)
+		#print(request.user.is_authenticated())
+
+		if user is not None:
+			#print(request.user.is_authenticated())
+			login(request,user)
+			return redirect("/login")
+		else:
+			print("Error")
+        	# Redirect to a success page.
+        	#context['form'] = LoginForm()
+        	# Return an 'invalid login' error message.
+	
+	return render(request,"auth/login.html",context)
+
+def register_page(request):
+	form = LoginForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+	return render(request,"auth/register.html",{})
