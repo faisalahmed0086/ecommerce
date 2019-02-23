dd6748b1853b64a90f4751f03dcf00bf0caec807
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-02-23 16:00:04 +0600

    custom model field

--- src/products/views.py
+++ src/products/views.py
@@ -8,13 +8,16 @@ from .models import Product
 # Create your views here.
 
 class ProductListView(ListView):
-	queryset = Product.objects.all()
 	template_name = "products/list.html"
 
 	# def get_context_data(self, *args, **kwargs):
 	# 	context = super(ProductListView, self).get_context_data(*args, **kwargs)
 	# 	print(context)
 	# 	return context
+
+	def get_queryset(self, *args, **kwargs):
+		request = self.request
+		return Product.objects.all()
 
 
 def product_list_view(request):
@@ -25,13 +28,26 @@ def product_list_view(request):
 	return render(request, "products/list.html", context)
 
 class ProductDetailView(DetailView):
-	queryset = Product.objects.all()
+	#queryset = Product.objects.all()
 	template_name = "products/detail.html"
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
 		print(context)
 		return context
+
+	def get_object(self, *args, **kwargs):
+		request = self.request
+		pk = self.kwargs.get('pk')
+		instance = Product.objects.get_by_id(pk)
+		if instance is None:
+			raise Http404("Product doesn't exist")
+		return instance
+
+	def get_queryset(self, *args, **kwargs):
+		request = self.request
+		pk = self.kwargs.get('pk')
+		return Product.objects.filter(pk=pk)
 
 
 
@@ -48,12 +64,17 @@ def product_detail_view(request, pk=None, *args, **kwargs):
 	# except:
 	# 	print("Huh?")
 
-	qs = Product.objects.filter(id=pk)
-	#print(qs)
-	if qs.exists() and qs.count() == 1:
-		instance = qs.first()
-	else:
+	instance = Product.objects.get_by_id(pk)
+	if instance is None:
 		raise Http404("Product doesn't exist")
+	# print(instance)
+	# qs = Product.objects.filter(id=pk)
+
+	# #print(qs)
+	# if qs.exists() and qs.count() == 1:
+	# 	instance = qs.first()
+	# else:
+	# 	raise Http404("Product doesn't exist")
 
 
 	context = {
