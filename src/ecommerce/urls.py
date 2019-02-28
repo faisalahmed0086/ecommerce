16cf6860ef043e54e06e7763162b4c884a693615
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-02-28 19:31:48 +0600

    slugfield and signals

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -24,6 +24,7 @@ from products.views import (
 		product_list_view, 
 		product_detail_view, 
 		ProductDetailView,
+		ProductDetailSlugView,
 		ProductFeaturedListView,
 		ProductFeaturedDetailView
 		)
@@ -40,7 +41,9 @@ urlpatterns = [
 	url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
 	url(r'^products/$', ProductListView.as_view()),
 	url(r'^products-fbv/$', product_list_view),
-	url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+	#url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+	url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
+
 	url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
