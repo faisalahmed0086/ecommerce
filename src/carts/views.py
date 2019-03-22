ae0cea975ff23eac042ed4c89c1ceb4e13e40e55
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-03-22 20:41:13 +0600

    Django session

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_home(request):
+	#print(request.session)
+	#print(dir(request.session))
+	#key = request.session.session_key
+	#print(key)
+	request.session['cart_id']= 12 # setter
+	request.session['user'] = request.user.username
 	return render(request, "carts/home.html", {})
