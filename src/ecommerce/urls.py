bd391bc6369dcf7d3eeb6d187ef59eecb1793f41
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-05-03 19:33:17 +0600

    from django.contrib import admin

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -1,5 +1,4 @@
 """ecommerce URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -13,6 +12,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -21,27 +21,25 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
-from accounts.views import login_page, register_page
+from accounts.views import login_page, register_page, guest_register_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
-	url(r'^$', home_page, name='home'),
-	url(r'^about/$', about_page, name='about'),
-	url(r'^contact/$', contact_page, name='contact'),
-	url(r'^login/$', login_page, name='login'),
-	url(r'^logout/$', LogoutView.as_view(), name='logout'),
-	#url(r'^cart/', include("carts.urls", namespace='cart')),
-	url(r'^cart/', include(('carts.urls', 'carts'), namespace='cart')),
-	#url(r'^cart/', include("carts.urls",'cart'), namespace='cart'),
-	url(r'^register/$', register_page, name='register'),
-	#url(r'^products/', include("products.urls", namespace='products')),
-	url(r'^products/', include(('products.urls', 'products'), namespace='products')),
-	#url(r'^search/', include("search.urls", namespace='search')),
-	url(r'^search/', include(('search.urls', 'search'), namespace='search')),
-	url(r'^bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/guest/$', guest_register_view, name='guest_register'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^cart/', include("carts.urls", namespace='cart')),
+    url(r'^register/$', register_page, name='register'),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
+    url(r'^products/', include("products.urls", namespace='products')),
+    url(r'^search/', include("search.urls", namespace='search')),
     url(r'^admin/', admin.site.urls),
 ]
 
+
 if settings.DEBUG:
-	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
