cd2b8c8e584145f8f94ea527cd62f76b846540e0
faisalahmed0086 <faisalahmed0086@gmail.com>
2019-07-06 13:35:33 +0600

    A basic selector

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -31,7 +31,7 @@ class UserAdminCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('full_name', 'email') #'full_name',)
+        fields = ('full_name', 'email',) #'full_name',)
 
     def clean_password2(self):
         # Check that the two password entries match
